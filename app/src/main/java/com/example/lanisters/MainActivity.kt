cab1bafa8d301d7a7739047cf802c9773e925907
Sushil19 <dubesushil99@gmail.com>
2020-06-19 19:42:25 +0530

    Waste management app

--- app/src/main/java/com/example/lanisters/MainActivity.kt
+++ app/src/main/java/com/example/lanisters/MainActivity.kt
@@ -2,11 +2,40 @@ package com.example.lanisters
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+        //return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+
+        if (id == R.id.action_one) {
+            Toast.makeText(this, "The scanner runs here", Toast.LENGTH_LONG).show()
+            return true
+        }
+        if (id == R.id.action_two) {
+            Toast.makeText(this, "Item Two Clicked", Toast.LENGTH_LONG).show()
+            return true
+        }
+        if (id == R.id.action_three) {
+            Toast.makeText(this, "Item Three Clicked", Toast.LENGTH_LONG).show()
+            return true
+        }
+
+    return super.onOptionsItemSelected(item)
     }
 }
