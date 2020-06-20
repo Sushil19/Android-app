3a3f36e644ae79decc2fd6611fb105a822b4991b
Sushil19 <dubesushil99@gmail.com>
2020-06-20 12:41:33 +0530

    scanner updated

--- app/src/main/java/com/example/lanisters/MainActivity.kt
+++ app/src/main/java/com/example/lanisters/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.lanisters
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
+import com.google.zxing.integration.android.IntentIntegrator
+import com.google.zxing.integration.android.IntentResult
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,7 +15,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
+
+
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu, menu)
@@ -23,8 +29,26 @@ class MainActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         val id = item.itemId
 
-        if (id == R.id.action_one) {
+        if (id == R.id.btn_scan) {
             Toast.makeText(this, "The scanner runs here", Toast.LENGTH_LONG).show()
+
+            val scanner = IntentIntegrator(this)
+
+            scanner.initiateScan()
+
+            fun onActivityResult(requestCode : Int, resultCode : Int, data : Intent?) {
+                val result : IntentResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+                if(result != null) {
+                    if(result.getContents() == null) {
+                        Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
+                    } else {
+                        Toast.makeText(this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
+                    }
+                } else {
+                    super.onActivityResult(requestCode, resultCode, data);
+                }
+            }
+
             return true
         }
         if (id == R.id.action_two) {
@@ -35,6 +59,7 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(this, "Item Three Clicked", Toast.LENGTH_LONG).show()
             return true
         }
+
 
     return super.onOptionsItemSelected(item)
     }
